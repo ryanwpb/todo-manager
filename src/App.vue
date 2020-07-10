413fc1f874cf8e1042ac396060b3bcf2eb65d149
Ryan Graham <ryangrahamwpb@gmail.com>
2020-07-10 10:11:59 -0400

    Add container element and style.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <Todos />
+    <div class="container">
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Todos from "./components/Todos.vue";
 
 export default {
   name: "App",
@@ -16,12 +18,24 @@ export default {
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background: #e8f7f0;
+  line-height: 1.6;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
